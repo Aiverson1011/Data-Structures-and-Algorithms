c8752fad183bd2697b282b077b113ae07cb9bf0a
aiverson1011 <ajohnson764@gmail.com>
2018-10-02 23:07:51 -0700

    Iterative approach

--- Challenges/BinarySearch/BinarySearch/Program.cs
+++ Challenges/BinarySearch/BinarySearch/Program.cs
@@ -9,9 +9,51 @@ namespace BinarySearch
 			Console.WriteLine("Hello World!");
 		}
 
-		static void BinarySearch()
+		/// <summary>
+		/// Iteratively get index from a sorted array using binary search
+		/// </summary>
+		/// <param name="binaryArray">sorted binary array</param>
+		/// <param name="value">value to search for</param>
+		/// <returns>the index of the array</returns>
+		static int BinarySearchIterative(int[] binaryArray, int value)
+		{
+			int minimum = 0;
+			int last = binaryArray.Length - 1;
+
+			while (minimum <= last)
+			{
+				//recalculate the middle portion
+				int middle = (minimum + last) / 2;
+
+				if (value > binaryArray[middle])
+				{
+					// if value is larger than the middle, then we increase
+					// minimum number to be one larger than the middle
+					minimum += middle++;
+
+				}
+				else if (value < binaryArray[middle])
+				{
+					// if the value is less than the middle, then place the last position
+					// to one minus the middle index. 
+					last = middle--;
+				}
+				else
+				{
+					// return the middle index + 1 to get the actual index number
+					return ++middle;
+				}
+			}
+
+			return -1;
+		}
+
+		static int BinarySearchRecursive(int[] binaryArray, int value)
 		{
 
+
+
+			return -1;
 		}
 	}
 }
